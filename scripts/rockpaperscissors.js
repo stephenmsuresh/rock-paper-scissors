ed1d6e410f88d8e227fad2b1ac8af9b1244bf401
Stephen Suresh <stephen.suresh@outlook.com>
2023-06-24 13:36:40 -0400

    add event listener to player selections

--- scripts/rockpaperscissors.js
+++ scripts/rockpaperscissors.js
@@ -1,3 +1,13 @@
+let playerScore = 0;
+let computerScore = 0;
+
+//.children property returns HTML collection, not an array
+const playerChoice = Array.from(document.getElementById('choices').children);
+
+playerChoice.forEach((choice) => {
+    choice.addEventListener('click', playRound);
+})
+
 //function to generate random computer "choice" of rock/paper/scissors
 function getComputerChoice() {
     //create an array to store values that will be randomly selected
@@ -12,46 +22,32 @@ function capitalize(word) {
     return ((word.toLowerCase()).replace(word.charAt(0), word.charAt(0).toUpperCase()));
 };
 
-function playRound(playerSelection, computerSelection) {
+function playRound(e) {
+    let playerSelection = this.id;
+    let computerSelection = getComputerChoice();
     if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-        return ("It's a tie!");
+        // return ("It's a tie!");
+        console.log("tie");
+        updateScore("tie");
     }
     else {
         if ((playerSelection.toLowerCase() == "rock" && computerSelection.toLowerCase() == "paper") ||
             (playerSelection.toLowerCase() == "scissors" && computerSelection.toLowerCase() == "rock") ||
             (playerSelection.toLowerCase() == "paper" && computerSelection.toLowerCase() == "scissors")) {
-            return (`You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}!`);
+            // return (`You lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}!`);
+            console.log("lose");
+            updateScore("lose")
         }
-        else{
-            return (`You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}!`);
+        else {
+            // return (`You win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}!`);
+            console.log("win");
+            updateScore("win");
         }
     }
 }
 
-function game(){
-    //number of rounds of rock paper scissors to play
-    let noOfGames = 5, playerScore = 0, computerScore = 0;
-    //loop that plays a certain number of rounds
-    for(let i = 1; i <= noOfGames; i++){
-        let playerSelection = prompt("Let's play a round of rock paper scissors! Enter your choice: ");
-        let result = playRound(playerSelection, getComputerChoice());
-        if(result.charAt(4) === 'w'){
-            playerScore++;
-        }
-        else if(result.charAt(4) == 'l'){
-            computerScore++;
-        }
-        console.log(result);
-    }
-    if(playerScore > computerScore){
-        console.log("Congratulations! You won!");
-    }
-    else if(computerScore > playerScore){
-        console.log("Oh no! You lost!");
-    }
-    else{
-        console.log("It's a tie!");
-    }
+function updateScore(result){
+
 }
 
-game();
+// game();
